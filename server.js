41950be3ad11185df341e46b8aebacffeeeceaa7
Dolev <dolev2466@gmail.com>
2020-10-05 16:37:45 +0300

    Create Dummy Routes For users, posts, profile, auth.

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const connectDB = require('./config/db');
-
 const app = express();
+
+const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 // Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 const PORT = process.env.PORT || 5000;
 
