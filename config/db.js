efa962cf06caa48bce78181b2270479d0df2615d
Dolev <dolev2466@gmail.com>
2020-10-06 02:46:50 +0300

    Add user registration

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ const dbURI = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbURI);
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
