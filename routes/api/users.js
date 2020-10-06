0bdaf21b92cce5bb5112318d09a3165db8d9cc2d
Dolev <dolev2466@gmail.com>
2020-10-06 03:01:37 +0300

    return WebToken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 //@route    POST api/users
@@ -54,7 +56,21 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      res.send('User registerd');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
