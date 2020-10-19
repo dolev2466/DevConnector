bdaa867a8c57370dc0a1f5fabc285c10290ffa92
Dolev <dolev2466@gmail.com>
2020-10-19 21:53:33 +0300

    Add Get all posts, get post by id, delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,7 +6,7 @@ const User = require('../../models/User');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 
-//@route    Post api/posts
+//@route    POST api/posts
 //@desc     Create a post
 //@access   Private
 router.post(
@@ -37,5 +37,63 @@ router.post(
     }
   }
 );
+
+//@route    GET api/posts
+//@desc     Get all posts
+//@access   Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    GET api/posts/:id
+//@desc     get post by id
+//@access   Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route    DELETE api/post/:id
+//@desc     Delete post
+//@access   Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'User not authorized' });
+    }
+    await post.remove();
+    res.json({ messgae: ' Post removed' });
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
